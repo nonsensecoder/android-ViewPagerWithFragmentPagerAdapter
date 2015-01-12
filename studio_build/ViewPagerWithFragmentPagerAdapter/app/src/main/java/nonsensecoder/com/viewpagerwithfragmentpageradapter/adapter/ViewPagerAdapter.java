c13634c263f059227a7d2668aab710c5c876dbd4
Ratul Ghosh <tibro4u@gmail.com>
2015-01-12 10:29:53 -0800

    Android Studio Project Added.

---
+++ studio_build/ViewPagerWithFragmentPagerAdapter/app/src/main/java/nonsensecoder/com/viewpagerwithfragmentpageradapter/adapter/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package nonsensecoder.com.viewpagerwithfragmentpageradapter.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ratul Ghosh on 1/7/2015.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final ArrayList<Fragment> fragments;
+
+    public ViewPagerAdapter(FragmentManager fm, ArrayList<Fragment> fragments) {
+        // mandatory to call the super() with a fragment manager
+        super(fm);
+        this.fragments = fragments;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        // returning fragment of particular page
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        // returning total number of page
+        return fragments.size();
+    }
+
+
+}
